b7940576b2c5fc6fe9a9e553ec5c644a6dda9246
Duckos-Mods <businwes@hotmail.com>
2023-12-03 06:12:48 +0000

    new printer

--- bettermolang/core.go
+++ bettermolang/core.go
@@ -1,5 +1,7 @@
 package bettermolang
 
+import "fmt"
+
 const testString string = `
 func rotate(inout x, inout y, a){
     ca = cos(a);
@@ -12,7 +14,7 @@ func square(x){
     return x*x;
 }
 
-var str = "stuff n () { things'
+var str = 'stuff n () { things'
 vec var p = (1,2)
 arr tmp a = [1,2,2,7,5]
 var px = a[3];
@@ -25,24 +27,32 @@ rotate(px,py,45.0);
 "(//filter this -> ))"
 '(stuff)'
 */
-
-//comment
 `
 
 const testString2 string = `
 ### This is a comment
 +- / * ### This is also a comment
 [{([{()}])}] ### This is also a comment
-'This is a string!!!!!!!!!'
-### Multi line string
+'This is a string!!!!!!!!! {ajuidhaiudhaiuhdiuahwiud} []() ******'
+### Multi line string 
 'THIS IS A MULTI LINE STRING
 THIS IS A MULTI LINE STRING
 THIS IS A MULTI LINE STRING'
+
+' An escaped string!\' '
 /* 
 This is a multi line comment
-Please fucking work
+Please fucking work 21312321 {ahduawhdu'dhwaiyudghuaiyghbdu '}
 */  
 --++*
+12.1 + 99
+
+func functionName() {
+    return 1 + 2
+}
+
+var b = 20.1
+var a = b + functionName()
 `
 
 const testString3 string = `
@@ -51,10 +61,14 @@ Test Comment Please Work You Fuckhead
 */
 `
 
+const testString4 string = `
+'123\\\'321'
+`
+
 func RunTest() {
 	scanner := NewScanner()
-	tokens := scanner.ScanTokens(testString2)
+	tokens := scanner.ScanTokens(testString)
 	for _, token := range tokens {
-		println(token.Value)
+		println(fmt.Sprintf("String: %s, ID: %d", token.Value, token.TokenType))
 	}
 }
